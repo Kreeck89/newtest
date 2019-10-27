72af43d7d0d48f8394658b83d1707cea3e7e19c9
kreeck89 <kreeckkreeck@gmail.com>
2019-10-27 16:20:24 +0200

    java doc

--- src/main/java/service/impl/SumString.java
+++ src/main/java/service/impl/SumString.java
@@ -2,6 +2,9 @@ package service.impl;
 
 import service.CountValue;
 
+/**
+ * Will write later
+ */
 public class SumString implements CountValue<String, String> {
     @Override
     public String count(String a, String b) {
