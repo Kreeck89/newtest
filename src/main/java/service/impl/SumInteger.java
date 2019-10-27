81068b47a84eb999844da61ab4b7446908fdedf9
kreeck89 <kreeckkreeck@gmail.com>
2019-10-27 16:08:53 +0200

    java docs

--- src/main/java/service/impl/SumInteger.java
+++ src/main/java/service/impl/SumInteger.java
@@ -2,6 +2,9 @@ package service.impl;
 
 import service.CountValue;
 
+/**
+ * Will write later
+ */
 public class SumInteger implements CountValue<Integer, Integer> {
     @Override
     public Integer count(Integer a, Integer b) {
